f3fa3958b6dcf011e303aa070d3bbb6c6055ca03
Chris Renshaw <104800006+renners7777@users.noreply.github.com>
2023-01-22 17:15:40 +0000

    Files deleted

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = new express();
-app.use(express.static('public'));
-const port = 8080;
-app.listen(port, ()=>{
-    console.log(`Serving photo app on http://localhost:${port}`)
-})
