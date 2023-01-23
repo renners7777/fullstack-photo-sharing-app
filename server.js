8a33d6b50cd0138c2a57a59452d00d4bd597cb4d
Chris Renshaw <104800006+renners7777@users.noreply.github.com>
2023-01-23 12:20:07 +0000

    Change made to photo.ejs + server.js

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-//imports
 const express = require("express");
-const db = require("./models"); //this line
-const app = express();
+const app = new express();
+const port = 8080;
+const db = require("./models");
+app.use(express.static("public"));
+app.listen(port, () => {
+    console.log(`Serving photo app on http://localhost:${port}`);
+});
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-
-// server start
-const port = 8080;
 const sqlPort = 3307;
-app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
-});
+// set the view engine to ejs
+app.set("view engine", "ejs");
 
-//routes
 
 //db
 db.sequelize
@@ -28,23 +27,17 @@ db.sequelize
         console.error("Unable to connect to the database:", error);
     });
 
-// set the view engine to ejs
-app.set("view engine", "ejs");
-// serve static files from public
-app.use(express.static('public'))
+const PhotosRouter = require('./routes/PhotosRouter')
+const CommentsRouter = require('./routes/CommentsRouter')
+const UsersRouter = require('./routes/UsersRouter')
+app.use("/images", PhotosRouter);
+app.use("/comments", CommentsRouter);
+app.use("/users", UsersRouter);
+
 // use res.render to load up an ejs template
 app.get("/", (req, res) => {
     res.render("index");
 });
-
-//server.js
-const PhotosRouter = require('./routes/PhotosRouter')
-const CommentsRouter = require('./routes/CommentsRouter')
-const UsersRouter = require('./routes/UsersRouter')
-app.use('/images', PhotosRouter)
-app.use('/comments', CommentsRouter)
-app.use('/users', UsersRouter)
-
 app.get("/photo", (request, response) => {
     console.log("/photo");
     response.render("photo");
